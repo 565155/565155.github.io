f6cbbd2e253c142952e2b0465ac2d226ad72fce3
565155 <565155@my.cuhsd.org>
2024-09-13 11:04:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function enableCSS() {
+  css = document.createElement("link");
+  css.rel = "stylesheet";
+  css.type = "text/css";
+  css.href = "style.css";
+  document.head.appendChild(css);
+}
